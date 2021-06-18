164fc3bf9e4b6eec6a2d13bc2299848b9a3c61bc
Tandy <tandyfw@gmail.com>
2021-06-18 17:28:50 -0300

    index.js porta

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config({ path: 'variables.env' });
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
