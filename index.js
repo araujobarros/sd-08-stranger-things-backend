f9a0fd6d7ea8b45f3899d066a7a46bfde1e63944
Thiago Ferreira <thiagocristhian@gmail.com>
2021-06-18 11:59:46 -0300

    Projeto Stranger Things 🔦🚲

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
 const app = express();
+
+const PORT = Number(process.env.PORT);
+const NOT_FOUND = 404;
+const OK = 200;
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -16,17 +21,21 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const { UPSIDEDOWN_MODE } = process.env;
 
 app.get('/', (req, res) => {
-  const characters = strangerThingsService.search(
-    req.query,
-    hereIsTheUpsideDown,
-  );
-
-  res.status(200).json(characters);
+  try {
+    const characters = strangerThingsService.search(
+      req.query,
+      Boolean(UPSIDEDOWN_MODE === 'true'),
+    );
+  
+    res.status(OK).json(characters);
+  } catch (error) {
+    res.status(NOT_FOUND).json({ message: error.message });
+  }
 });
 
-app.listen(3000, () => {
-  console.log('Escutando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Escutando na porta ${PORT}`);
 });
