1b8508e99de47f15be30fc0ea1c800ac925cd625
Daniel <danieldsrd@gmail.com>
2021-06-18 15:53:04 -0300

    .env

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
+const { PORT } = process.env;
 const app = express();
-const PORT = process.env.PORT || 3000;
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE || true;
+
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE || 'false';
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
