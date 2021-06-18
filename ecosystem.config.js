c3712973876eee063f9583f8a553c13ab470b726
hugobrr <hugobrr@gmail.com>
2021-06-18 15:53:38 -0300

    corrigindo o lint 3

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,25 +1,26 @@
 module.exports = {
-  apps : [{
+  apps: [{
     name: 'Stranger_things',
     instaces: 2,
     script: 'index.js',
     watch: 'false',
-    exec_mode: 'cluster'
+    // eslint-disable-next-line camelcase
+    exec_mode: 'cluster',
   }, {
     script: './service-worker/',
-    watch: ['./service-worker']
+    watch: ['./service-worker'],
   }],
 
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
+  deploy: {
+    production: {
+      user: 'SSH_USERNAME',
+      host: 'SSH_HOSTMACHINE',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+    },
+  },
 };
