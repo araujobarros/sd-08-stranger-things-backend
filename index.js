684d90e028e6e513f55eb242fdca445c76abf835
Anderson Santana <anderslash@gmail.com>
2021-06-18 20:48:12 -0300

    fix: desestrutura process.env

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 require('dotenv').config();
 
-const { PORT } = process.env;
+const { PORT, UPSIDEDOWN_MODE } = process.env;
 
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
@@ -20,7 +20,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'false';
+const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'true';
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
