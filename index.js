8c8767699fd1c30e4fbd20dd5b2f248ac98834c7
VitorLimaRios <vitorlimarios999@gmail.com>
2021-06-21 19:26:15 -0300

    upside down

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = JSON.parse(process.env.UPSIDEDOWN_MODE) || false;
+const hereIsTheUpsideDown = JSON.parse(process.env.UPSIDEDOWN_MODE) || true;
 const PORT = JSON.parse(process.env.PORT) || 3000;
 
 app.get('/', (req, res) => {
