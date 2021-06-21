d4435453aa5675baf51bb6721f6d15f32b2ad1e1
Tiago <tiagaoalb@gmail.com>
2021-06-21 12:44:08 -0300

    teste

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true';
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  const characters = strangerThingsService.search(
+  const character = strangerThingsService.search(
     req.query,
     hereIsTheUpsideDown,
   );
 
-  res.status(200).json(characters);
+  res.status(200).json(character);
 });
 
 app.listen(PORT, () => {
