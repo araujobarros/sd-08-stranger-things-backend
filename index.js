c50c07623bfe1539cfbb3162e76bbd75916aefef
devFelipeFranca <felipefrancafj@gmail.com>
2021-06-27 15:47:24 -0300

    Update

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const StrangerThingsService = require('./services/StrangerThings');
 const app = express();
 
 const strangerThingsRepository = new StrangerThingsRepository(
-  strangerThingsDataset,
+  strangerThingsDataset
 );
 const strangerThingsService = new StrangerThingsService(
-  strangerThingsRepository,
+  strangerThingsRepository
 );
 
 app.use(cors());
@@ -21,12 +21,14 @@ const hereIsTheUpsideDown = true;
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
-    hereIsTheUpsideDown,
+    hereIsTheUpsideDown
   );
 
   res.status(200).json(characters);
 });
 
-app.listen(process.env.PORT || 3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
