a9ae3f05276bc8ce6b7915573a8bd5aa32621f6a
devFelipeFranca <felipefrancafj@gmail.com>
2021-06-27 16:41:10 -0300

    Update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE === 'true' || true;
+const hereIsTheUpsideDown = false;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
