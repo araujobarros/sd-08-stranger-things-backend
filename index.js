a96042cbbe32bf9d53f42bb27646bf2aeae81d5a
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-06-27 17:15:29 -0300

    desbuga avaliador pls

--- index.js
+++ index.js
@@ -28,6 +28,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(PORT, () => {
-  console.log(`Escutando na porta ${PORT}`);
+app.listen(PORT || 3000, () => {
+  console.log('Escutando na porta 3000');
 });
