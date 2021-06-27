6fb6ce5ccd8854e28622d619da6cbb279e1c5282
devFelipeFranca <felipefrancafj@gmail.com>
2021-06-26 21:58:38 -0300

    Update

--- index.js
+++ index.js
@@ -28,9 +28,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log(
-    'Express server listening on port %d in %s mode',
-    this.address().port,
-    app.settings.env,
-  );
+  console.log('Escutando na porta 3000');
 });
