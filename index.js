29ac56e808fb6e8483c97c7069703103b38ca3e2
Thais Quintela <thais.s.quintela@gmail.com>
2021-06-28 11:45:12 -0300

    heroku bk

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 
+// https://blog.rocketseat.com.br/variaveis-ambiente-nodejs/
+require('dotenv').config();
+
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
