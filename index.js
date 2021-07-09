809d0dbd032e205853308610e5c8e9a3055621ec
caiomzrocha <caiomzrocha@gmail.com>
2021-07-08 23:05:23 -0300

    [FIX] Procfile

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const strangerThingsDataset = require('./data/dataset/stranger-things-characters
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3000;
 const { UPSIDEDOWN_MODE } = process.env;
 
 const app = express();
