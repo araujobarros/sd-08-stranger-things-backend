5f039568b354c5434495fa1397a017578cc0e6c0
caiomzrocha <caiomzrocha@gmail.com>
2021-07-08 22:18:51 -0300

    [FIX]

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ const strangerThingsDataset = require('./data/dataset/stranger-things-characters
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
 
-const app = express();
 const PORT = process.env.PORT || 3002;
 const { UPSIDEDOWN_MODE } = process.env;
+
+const app = express();
+app.use(cors());
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -16,8 +18,6 @@ const strangerThingsRepository = new StrangerThingsRepository(
 const strangerThingsService = new StrangerThingsService(
   strangerThingsRepository,
 );
-
-app.use(cors());
 
 const hereIsTheUpsideDown = UPSIDEDOWN_MODE === 'true';
 
