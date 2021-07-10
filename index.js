2501922ff853efe0a3cd4319bf64adbdba98317d
wagnerberna <cabelo29007@gmail.com>
2021-07-10 06:41:42 -0300

    requisitos iniciais

--- index.js
+++ index.js
@@ -4,8 +4,15 @@ const cors = require('cors');
 const strangerThingsDataset = require('./data/dataset/stranger-things-characters.json');
 const StrangerThingsRepository = require('./data/repository/StrangerThings');
 const StrangerThingsService = require('./services/StrangerThings');
-// inicio
+
 const app = express();
+
+// REQ01
+require('dotenv').config();
+
+const PORT = Number(process.env.PORT) || 3000;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
+// FIM
 
 const strangerThingsRepository = new StrangerThingsRepository(
   strangerThingsDataset,
@@ -16,8 +23,6 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
-
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
     req.query,
@@ -27,6 +32,6 @@ app.get('/', (req, res) => {
   res.status(200).json(characters);
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Escutando na porta 3000');
 });
