1f90a3dcd07064c636024f69f75b404988e1888f
wagnerberna <cabelo29007@gmail.com>
2021-07-12 11:41:16 -0300

    projeto back

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 // REQ01
 require('dotenv').config();
 
-const PORT = Number(process.env.PORT) || 3000;
+const PORT = process.env.PORT || 3000;
 const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 // FIM.
 
